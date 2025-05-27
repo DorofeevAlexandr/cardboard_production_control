10c5fb043143a29cedb8617032c6636fba589039
DorofeevAlexandr <amd_86@mail.ru>
2025-05-27 14:46:38 +0300

    Создана Админ панель

--- Backend/cardboard_production/admin.py
+++ Backend/cardboard_production/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Productions, Order, Material
 
-# Register your models here.
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    # Отображение полей в списке
+    list_display = ('name', )
+    # Фильтрация в списке
+    list_filter = ('name',)
+    # Поиск по полям
+    search_fields = ('name',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    # Отображение полей в списке
+    list_display = ('name', 'format', 'profile', 'material_outer', 'material_corrugation', 'material_inside')
+    # Фильтрация в списке
+    list_filter = ('name', 'format',)
+    # Поиск по полям
+    search_fields = ('name', 'format',)
+
+@admin.register(Productions)
+class ProductionsAdmin(admin.ModelAdmin):
+    # Отображение полей в списке
+    list_display = ('order_date', 'order', 'quantity',)
+    # Фильтрация в списке
+    list_filter = ('order_date', 'order')
+    # Поиск по полям
+    search_fields = ('order_date', 'order')
