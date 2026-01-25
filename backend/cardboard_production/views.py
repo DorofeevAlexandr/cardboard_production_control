050c917cc30b29bf8360c00ef0ec5c2f4b243ed9
DorofeevAlexandr <amd_86@mail.ru>
2026-01-25 18:03:34 +0300

    Устанавливаем динамически тип файла

--- backend/cardboard_production/views.py
+++ backend/cardboard_production/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import (HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect,
                          HttpResponsePermanentRedirect, FileResponse)
 
+import mimetypes
+
 
 def tuning(request):
     return HttpResponse("<h1>Tuning</h1>")
@@ -13,8 +15,12 @@ def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
 def open_pdf_file_view(request, filename):
+    mimetype = mimetypes.guess_type(filename)
+    app_type = mimetype[0]
+    app_type = 'application/pdf'
     data = {
         'title': filename,
+        'app_type': app_type,
         'filename': f'/{filename}',
     }
     return render(request, 'cardboard_production/open_pdf.html', context=data)
