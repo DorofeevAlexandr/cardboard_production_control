ce9ac5284106afb74f8c8a4a0cb9138cb9452123
DorofeevAlexandr <amd_86@mail.ru>
2026-04-22 22:14:30 +0300

    Добавил поток чтения данных счетчиков через адаптер

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 import time
 
+import adapters
 from time_series_base import client_influxdb, write_electro_counters_values, read_electro_counters_values
 from wear_lines import (read_electro_counters_params_in_base, read_electro_counters_update_in_base)
 
@@ -27,12 +28,12 @@ def postgres_engine():
 
 def read_counters_save_current_params(p_engine):
     with Session(autoflush=False, bind=p_engine) as db:
-        registers = [n for n in range(10)]
+        # registers = [n for n in range(10)]
+        registers = adapters.get_registers()
         print(registers)
         counters_params = read_electro_counters_params_in_base(session=db)
         print(counters_params)
         read_electro_counters_update_in_base(db, counters_params, registers)
-
 
         try:
             pass
@@ -47,7 +48,7 @@ if __name__ == '__main__':
     dt_last_save_current_params = dt.datetime.now()
     dt_last_save_lengths = dt.datetime.now()
     while True:
-        time.sleep(1)
+        time.sleep(60)
         if dt.datetime.now() - dt_last_save_current_params >= dt.timedelta(seconds=1):
             counters_params = read_counters_save_current_params(p_engine=engine)
             if counters_params:
